741a36d553c996ea13aae8ea4081838db1201b12
Aemiser <1530192616@qq.com>
2026-01-21 00:20:59 +0800

    feat-用户登录日志表结构的搭建

--- models/log_model.go
+++ models/log_model.go
@@ -3,12 +3,12 @@ package models
 type LogModel struct {
 	Model
 	LogType   int8      `json:"logType"` // 日志类型 1 2 3
-	Title     string    `json:"title"`
+	Title     string    `gorm:"size:64" json:"title"`
 	Content   string    `json:"content"`
 	Level     int8      `json:"level"` // 日志等级 1 2 3
 	UserID    uint      `json:"userID"`
 	UserModel UserModel `gorm:"foreignKey:UserID" json:"-"`
-	IP        string    `json:"ip"`
-	Addr      string    `json:"addr"`
+	IP        string    `gorm:"size:32" json:"ip"`
+	Addr      string    `gorm:"size:64" json:"addr"`
 	IsRead    bool      `json:"isRead"` //是否读取
 }
