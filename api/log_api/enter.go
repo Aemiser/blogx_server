7b2163ca95bcaed1d1a5f86448d3c9aa46c5ae20
Aemiser <1530192616@qq.com>
2026-01-23 23:46:38 +0800

    feat-API日志管理·通用列表查询封装

--- api/log_api/enter.go
+++ api/log_api/enter.go
@@ -1,9 +1,10 @@
 package log_api
 
 import (
+	"blogx_server/common"
 	"blogx_server/common/res"
-	"blogx_server/global"
 	"blogx_server/models"
+	"blogx_server/models/enum"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
@@ -13,9 +14,19 @@ type LogApi struct {
 }
 
 type LogListRequest struct {
-	Limit int    `form:"limit" json:"limit"`
-	Page  int    `form:"page" json:"page"`
-	Key   string `form:"key" json:"key"`
+	common.PageInfo
+	LogType     enum.LogType      `form:"logType" json:"logType"`
+	Level       enum.LogLevelType `form:"level" json:"level"`
+	UserID      uint              `form:"userID" json:"userID"`
+	IP          string            `form:"ip" json:"ip"`
+	LoginStatus bool              `form:"loginStatus" json:"loginStatus"`
+	ServiceName string            `form:"serviceName" json:"serviceName"`
+}
+
+type LogListResponse struct {
+	models.LogModel
+	UserNickName string `json:"userNickName"`
+	UserAvatar   string `json:"userAvatar"`
 }
 
 func (LogApi) LogListView(c *gin.Context) {
@@ -26,25 +37,61 @@ func (LogApi) LogListView(c *gin.Context) {
 		res.FailWithError(err, c)
 		return
 	}
+
 	fmt.Println(req)
+	list, count, err := common.ListQuery(models.LogModel{
+		LogType:     req.LogType,
+		Level:       req.Level,
+		UserID:      req.UserID,
+		IP:          req.IP,
+		LoginStatus: req.LoginStatus,
+		ServiceName: req.ServiceName,
+	}, common.Options{
 
-	var List []models.LogModel
-	if req.Page >= 20 {
-		req.Page = 1
+		PageInfo:     req.PageInfo,
+		Preloads:     []string{"UserModel"},
+		Likes:        []string{"Title"},
+		Debug:        true,
+		DefaultOrder: "created_at desc",
+	},
+	)
+	//
+	//var List []models.LogModel
+	//if req.Page >= 20 {
+	//	req.Page = 1
+	//}
+	//if req.Page <= 0 {
+	//	req.Page = 1
+	//}
+	//
+	//if req.Limit == 0 || req.Limit > 100 {
+	//	req.Limit = 10
+	//}
+	//offest := (req.Page - 1) * req.Limit
+	//model := models.LogModel{
+	//	LogType:     req.LogType,
+	//	Level:       req.Level,
+	//	UserID:      req.UserID,
+	//	IP:          req.IP,
+	//	LoginStatus: req.LoginStatus,
+	//	ServiceName: req.ServiceName,
+	//}
+	//
+	//like := global.Db.Debug().Where("title like ?", fmt.Sprintf("%%%s%%", req.Key))
+	//global.Db.Debug().Preload("UserModel").Where(like).Where(model).Offset(offest).Limit(req.Limit).Find(&List)
+	//
+	//var count int64
+	//global.Db.Debug().Where(like).Where(model).Model(&models.LogModel{}).Count(&count)
+
+	var _list = make([]LogListResponse, 0)
+	for _, logModel := range list {
+		_list = append(_list, LogListResponse{
+			LogModel:     logModel,
+			UserNickName: logModel.UserModel.Nickname,
+			UserAvatar:   logModel.UserModel.Avatar,
+		})
+
 	}
-	if req.Page <= 0 {
-		req.Page = 1
-	}
-
-	if req.Limit == 0 || req.Limit > 100 {
-		req.Limit = 10
-	}
-	offest := (req.Page - 1) * req.Limit
-	global.Db.Debug().Offset(offest).Limit(req.Limit).Find(&List)
-
-	var count int64
-	global.Db.Debug().Model(&models.LogModel{}).Count(&count)
-
-	res.FailWithList(List, int(count), c)
+	res.FailWithList(_list, int(count), c)
 	return
 }
---
+++ common/list_query.go
@@ -0,0 +1,94 @@
+package common
+
+import (
+	"blogx_server/global"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type PageInfo struct {
+	Limit int    `form:"limit" json:"limit"`
+	Page  int    `form:"page" json:"page"`
+	Key   string `form:"key" json:"key"`
+	Order string `form:"order" json:"order"`
+}
+
+func (p PageInfo) GetLimit() int {
+	if p.Limit >= 20 || p.Limit <= 0 {
+		p.Limit = 1
+	}
+	return p.Limit
+}
+
+func (p PageInfo) GetPage() int {
+	if p.Page >= 20 || p.Page <= 0 {
+		p.Page = 1
+	}
+	return p.Page
+}
+
+func (p PageInfo) GetOffset() int {
+	return (p.GetPage() - 1) * p.GetLimit()
+}
+
+type Options struct {
+	PageInfo     PageInfo
+	Likes        []string
+	Preloads     []string
+	Where        *gorm.DB
+	Debug        bool
+	DefaultOrder string
+}
+
+func ListQuery[T any](model T, options Options) (list []T, count int, err error) {
+	var query = global.Db.Model(model).Where(model)
+
+	// 显示日志
+	if options.Debug {
+		query = query.Debug()
+	}
+
+	// 模糊查询
+	if len(options.Likes) > 0 && options.PageInfo.Key != "" {
+		fmt.Println("模糊查询列:", options.Likes)
+		likes := global.Db.Where("")
+		for _, column := range options.Likes {
+			likes.Or(
+				fmt.Sprintf("%s like ?", column),
+				fmt.Sprintf("%%%s%%", options.PageInfo.Key),
+			)
+		}
+		query = query.Where(likes)
+	}
+
+	// 定制化查询
+	if options.Where != nil {
+		query = query.Where(options.Where)
+	}
+
+	// 预加载
+	for _, preload := range options.Preloads {
+		query = query.Preload(preload)
+	}
+	// 查总数
+	var _count int64
+	query.Count(&_count)
+	count = int(_count)
+
+	// 分页
+	offest := options.PageInfo.GetOffset()
+	limit := options.PageInfo.GetLimit()
+
+	// 排序
+	if options.PageInfo.Order != "" {
+		query = query.Order(options.PageInfo.Order)
+	} else {
+		if options.DefaultOrder != "" {
+			query = query.Order(options.DefaultOrder)
+		}
+	}
+
+	err = query.Offset(offest).Limit(limit).Find(&list).Error
+	return
+}
