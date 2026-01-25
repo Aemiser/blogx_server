08dec4a3c53a721dbe54b2db3d92d8e772c0251f
Aemiser <1530192616@qq.com>
2026-01-25 21:44:45 +0800

    feat-图片管理·文件名使用hash存储

--- api/image_api/image_upload.go
+++ api/image_api/image_upload.go
@@ -3,12 +3,15 @@ package image_api
 import (
 	"blogx_server/common/res"
 	"blogx_server/global"
+	"blogx_server/models"
 	"blogx_server/utils"
 	"fmt"
+	"io"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 func (ImageApi) ImageUploadView(c *gin.Context) {
@@ -17,7 +20,7 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 		res.FailWithError(err, c)
 		return
 	}
-
+	filename := fileHeader.Filename
 	confSize := global.Config.Uploads.Size              // 从配置项读取大小
 	confSizeType := global.Config.Uploads.GetSizeType() // 从配置项读取类型
 	if fileHeader.Size >= confSize*confSizeType {
@@ -25,26 +28,56 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 		return
 	}
 
-	err = imageSuffixJudgment(fileHeader.Filename, global.Config.Uploads.WriteList)
+	suffix, err := imageSuffixJudgment(filename, global.Config.Uploads.WriteList)
 	if err != nil {
 		res.FailWithError(err, c)
 		return
 	}
 
-	filePath := fmt.Sprintf("/uploads/images/%s", fileHeader.Filename)
+	file, err := fileHeader.Open()
+	if err != nil {
+		res.FailWithError(err, c)
+		return
+	}
+	byteData, _ := io.ReadAll(file)
+	hash := utils.Md5(byteData)
+	filePath := fmt.Sprintf("uploads/%s/%s.%s", global.Config.Uploads.ImageDir, hash, suffix)
+	// 判断hash是否在库中
+	var model models.ImageModel
+	err = global.Db.Take(&model, "hash = ?", hash).Error
+	if err == nil {
+		// 说明存在
+		logrus.Infof("上传的图片重复了 %s<==>%s", filename, hash)
+		res.Success(model.Path, "图片上传成功", c)
+		return
+	}
+	// 入库
+	err = global.Db.Create(&models.ImageModel{
+		Filename: fileHeader.Filename,
+		Path:     filePath,
+		Size:     fileHeader.Size,
+		Hash:     hash,
+	}).Error
+	if err != nil {
+		logrus.Infof("数据库创建图片失败: %v", err)
+		res.FailWithError(err, c)
+		return
+	}
 	c.SaveUploadedFile(fileHeader, filePath)
-	res.Success(filePath, "上传成功", c)
+	res.Success(filePath, "图片上传成功", c)
 }
 
-func imageSuffixJudgment(fileName string, list []string) error {
+func imageSuffixJudgment(fileName string, list []string) (suffix string, err error) {
 	_list := strings.Split(fileName, ".")
 	if len(_list) == 1 {
-		return errors.New("文件格式错误")
+		err = errors.New("文件格式错误")
+		return
 	}
-	suffix := _list[len(_list)-1]
+	suffix = _list[len(_list)-1]
 	if !utils.InList(list, suffix) {
-		return errors.New("文件非法")
+		err = errors.New("文件非法")
+		return
 	}
-	return nil
+	return
 
 }
--- utils/enter.go
+++ utils/enter.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
 func InList[T comparable](list []T, value T) bool {
 	for _, v := range list {
 		if v == value {
@@ -7,4 +12,10 @@ func InList[T comparable](list []T, value T) bool {
 		}
 	}
 	return false
+}
+
+func Md5(data []byte) string {
+	md5New := md5.New()
+	md5New.Write(data)
+	return hex.EncodeToString(md5New.Sum(nil))
 }
