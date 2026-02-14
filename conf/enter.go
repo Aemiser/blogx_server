1ff0a2ddd6ff1fc2ce2073c0fb14a1e8248b84c8
Aemiser <1530192616@qq.com>
2026-02-14 20:54:45 +0800

    feat-主从数据库优化

--- conf/enter.go
+++ conf/enter.go
@@ -5,8 +5,7 @@ type Config struct {
 	Jwt     Jwt     `yaml:"jwt"`
 	Log     Log     `yaml:"log"`
 	Redis   Redis   `yaml:"redis"`
-	DB      DB      `yaml:"db"`
-	DB1     DB      `yaml:"db1"`
+	DB      []DB    `yaml:"db"`
 	Site    Site    `yaml:"site"`
 	Email   Email   `yaml:"email"`
 	QQ      QQ      `yaml:"qq"`
--- core/init_db.go
+++ core/init_db.go
@@ -3,17 +3,21 @@ package core
 import (
 	"blogx_server/global"
 	"fmt"
+	"time"
+
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/plugin/dbresolver"
-	"time"
 )
 
 func InitDB() *gorm.DB {
-	dc := global.Config.DB   //读
-	dc1 := global.Config.DB1 //写
+	if len(global.Config.DB) == 0 {
+		logrus.Fatalf("数据库配置为空")
+		return nil
+	}
 
+	dc := global.Config.DB[0] //读
 	//TODO pgsql的支持
 
 	db, err := gorm.Open(mysql.Open(dc.GetDSN()), &gorm.Config{
@@ -24,19 +28,23 @@ func InitDB() *gorm.DB {
 	}
 	sqlDB, err := db.DB()
 	// SetMaxIdleConns 设置空闲连接池中连接的最大数量。
-	sqlDB.SetMaxIdleConns(global.Config.DB.MaxIdleConns)
+	sqlDB.SetMaxIdleConns(dc.MaxIdleConns)
 	// SetMaxOpenConns 设置打开数据库连接的最大数量。
-	sqlDB.SetMaxOpenConns(global.Config.DB.MaxOpenConns)
+	sqlDB.SetMaxOpenConns(dc.MaxOpenConns)
 	// SetConnMaxLifetime 设置了可以重新使用连接的最大时间。
 	sqlDB.SetConnMaxLifetime(time.Hour)
 	logrus.Infof("数据库连接成功")
 
-	if !dc.Empty() {
+	// 读写库存在，则配置读写分离
+	if len(global.Config.DB) > 1 {
 		fmt.Println("读写库存在")
-		// 读写库存在，则配置读写分离
+		readList := []gorm.Dialector{}
+		for _, d := range global.Config.DB[1:] {
+			readList = append(readList, mysql.Open(d.GetDSN()))
+		}
 		db.Use(dbresolver.Register(dbresolver.Config{
-			Sources:  []gorm.Dialector{mysql.Open(dc1.GetDSN())}, // 写
-			Replicas: []gorm.Dialector{mysql.Open(dc.GetDSN())},  // 读
+			Sources:  []gorm.Dialector{mysql.Open(dc.GetDSN())}, // 写
+			Replicas: readList,                                  // 读
 			Policy:   dbresolver.RandomPolicy{},
 		}))
 	}
