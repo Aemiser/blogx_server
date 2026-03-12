dcac90cccbeb53987ffeecc9ac660834d9be3e7a
Aemiser <1530192616@qq.com>
2026-03-12 17:54:00 +0800

    文章管理-文章浏览

---
+++ api/article_api/article_look.go
@@ -0,0 +1,61 @@
+package article_api
+
+import (
+	"blogx_server/common/jwts"
+	"blogx_server/common/res"
+	"blogx_server/global"
+	"blogx_server/middlerware"
+	"blogx_server/models"
+	"blogx_server/models/enum"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ArticleLookRequest struct {
+	ArticleID  uint `json:"articleID" binding:"required"`
+	TimeSecond int  `json:"timeSecond"` //读文章一共用了多少时间
+}
+
+func (ArticleApi) ArticleLookView(c *gin.Context) {
+	cr := middlerware.GetBind[ArticleLookRequest](c)
+
+	// TODO:未登录的用户，浏览量怎么增加
+	claims, err := jwts.ParseTokenByGin(c)
+	if err != nil {
+		res.SuccessWithData("登入成功", c)
+		return
+	}
+
+	var article models.ArticleModel
+	// 检查文章是否存在，判断文章状态
+	err = global.Db.Take(&article, "status = ? and id =?", enum.ArticlePublished, cr.ArticleID).Error
+	if err != nil {
+		res.FailWithMsg("文章不存在", c)
+		return
+	}
+
+	// TODO：加入缓存，将该文章的id和用户id存入redis中
+	// 查这个文章今天有没有在足迹里面
+	var history models.UserArticleLookHistoryModel
+	err = global.Db.Take(&history, "article_id = ? and user_id = ? and created_at > ? and created_at <",
+		cr.ArticleID,
+		claims.Claims.UserID,
+		time.Now().Format("2006-01-02 15:04:05")+" 00:00:00",
+		time.Now().Format("2006-01-02")).Error
+	if err == nil {
+		res.SuccessWithData("成功", c)
+		return
+	}
+
+	err = global.Db.Create(&models.UserArticleLookHistoryModel{
+		UserID:    claims.Claims.UserID,
+		ArticleID: cr.ArticleID,
+	}).Error
+	if err != nil {
+		res.FailWithMsg("失败", c)
+		return
+	}
+	res.SuccessWithMsg("成功", c)
+	return
+}
--- router/article_router.go
+++ router/article_router.go
@@ -18,4 +18,5 @@ func ArticleRouter(r *gin.RouterGroup) {
 	r.POST("article/examine", middlerware.AdminMiddleware, middlerware.BindJsonMiddlerware[article_api.ArticleExamineRequest], app.ArticleExamineView)
 	r.GET("article/digg/:id", middlerware.AuthMiddleware, middlerware.BindUriMiddlerware[models.IDRequest], app.ArticleDiggView)
 	r.POST("article/collect", middlerware.AuthMiddleware, middlerware.BindJsonMiddlerware[article_api.ArticleCollectRequest], app.ArticleCollectView)
+	r.POST("article/look", middlerware.BindJsonMiddlerware[article_api.ArticleLookRequest], app.ArticleLookView)
 }
