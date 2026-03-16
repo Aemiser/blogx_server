529de6195ac8d76c9ec9632ccd1dd0c283f60b53
Aemiser <1530192616@qq.com>
2026-03-16 17:42:26 +0800

    feat-评论管理·评论树APi

--- router/comment_router.go
+++ router/comment_router.go
@@ -4,6 +4,7 @@ import (
 	"blogx_server/api"
 	"blogx_server/api/comment_api"
 	"blogx_server/middlerware"
+	"blogx_server/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +12,5 @@ import (
 func CommentRouter(r *gin.RouterGroup) {
 	app := api.App.CommentApi
 	r.POST("comment", middlerware.AuthMiddleware, middlerware.BindJsonMiddlerware[comment_api.CommentCreateReaquest], app.CommentListView)
+	r.GET("comment/tree/:id", middlerware.AuthMiddleware, middlerware.BindUriMiddlerware[models.IDRequest], app.CommentTreeView)
 }
