8843aae0528bf40b51a1d695ba6456d624924ca1
Aemiser <1530192616@qq.com>
2026-03-24 16:07:01 +0800

    feat-私信·在线用户记录

--- api/chat_api/chat_ws.go
+++ api/chat_api/chat_ws.go
@@ -8,12 +8,20 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
 )
 
 var UP = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+
+//	==> websocket地址 ==> websocket连接
+//
+// 用户id 	==> websocket地址 ==> websocket连接
+//
+//	==> websocket地址 ==> websocket连接
+var OnlineMap = map[uint]map[string]*websocket.Conn{}
 
 func (ChatApi) ChatView(c *gin.Context) {
 	// 用户认证
@@ -26,9 +34,26 @@ func (ChatApi) ChatView(c *gin.Context) {
 	// 服务升级
 	conn, err := UP.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		fmt.Println(err)
+		logrus.Errorf("ws服务升级失败：%v", err)
 		return
 	}
+
+	userID := claims.Claims.UserID
+	addr := conn.RemoteAddr().String()
+	addrMap, ok := OnlineMap[userID]
+	if !ok { // 不存在即创建
+		OnlineMap[userID] = map[string]*websocket.Conn{
+			addr: conn,
+		}
+
+	} else { // 存在，但地址对应的连接不存在即追加
+		_, ok1 := addrMap[addr]
+		if !ok1 {
+			OnlineMap[userID][addr] = conn
+		}
+	}
+	fmt.Println("进入", OnlineMap)
+
 	for {
 		// 消息类型，消息，错误
 		t, p, err := conn.ReadMessage()
@@ -43,5 +68,17 @@ func (ChatApi) ChatView(c *gin.Context) {
 		fmt.Println(t, string(p))
 	}
 	defer conn.Close()
+
+	addrMap2, ok2 := OnlineMap[userID]
+	if ok2 {
+		_, ok3 := addrMap2[addr]
+		if ok3 {
+			delete(OnlineMap[userID], addr)
+		}
+		if len(OnlineMap) == 0 {
+			delete(OnlineMap, userID)
+		}
+	}
+	fmt.Println("离开", OnlineMap)
 	fmt.Println("服务关闭")
 }
