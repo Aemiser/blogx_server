072f6ed53e4c4715276f8977d9219dee29dd5330
Aemiser <1530192616@qq.com>
2026-04-07 17:13:42 +0800

    main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"blogx_server/common/res"
+	"blogx_server/core"
+	"blogx_server/flags"
+	"blogx_server/global"
+	"blogx_server/router"
+)
+
+func main() {
+	flags.Parse()
+	global.Config = core.ReadConf()
+	core.InitLogrus()
+	global.Db = core.InitDB()
+	global.Redis = core.InitRedis()
+	global.ESClient = core.EsConnect()
+	flags.Run()
+
+	core.InitMysqlEs()
+
+	res.InitSysCode()
+	res.InitUserCode()
+	res.InitChatCode()
+	res.InitImageCode()
+	res.InitArticleCode()
+	res.InitFocusCode()
+
+	router.Run()
+}
